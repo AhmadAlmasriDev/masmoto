5edb4a8575f073139551fb59d79f2d1a75c10566
AhmadAlmasriDev <>
2023-06-26 15:35:13 +0000

    Add js file for paint page and body paint fuctionality

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,8 +1,22 @@
-document.addEventListener("DOMContentLoaded", paint);
+document.addEventListener("DOMContentLoaded", listeners);
 
-function paint() {
+function listeners() {
+    let bodyColor = document.getElementById("body-color");
+    let stripeColor = document.getElementById("stripe-color");
+    bodyColor.addEventListener("input", paintBody);
+}
+
+
+
+function paintBody() {
     let parts = document.getElementsByClassName("body");
     for (let part of parts) {
-        part.style = "color:#0dff00cc;";
+        part.style = `color:${this.value}cc;`;
+    }
+}
+function paintstripe() {
+    let stripes = document.getElementsByClassName("body");
+    for (let stripe of stripes) {
+        stripe.style = `color:${this.value};`;
     }
 }
