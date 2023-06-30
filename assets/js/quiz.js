98e2fb25b8c354f1881ce5c25a75fe099902821b
AhmadAlmasriDev <>
2023-06-30 17:22:30 +0000

    Add question load functionality

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,16 +1,16 @@
-const APILINK = "https://mocki.io/v1/5fb9ecc7-22b4-4ce0-91e9-f052d5c81b35";
+const APILINK = "https://mocki.io/v1/33f0ff85-bfa9-4d5b-99f9-1034af1a1a9e";
 
 document.addEventListener("DOMContentLoaded", test);
 
 function test() {
-    // debugger;
-    gameset(true);
+
+    gameSet(true);
 
 }
 
 
 
-function gameset(res) {
+function gameSet(res) {
     const options = {
         once: true,
     };
@@ -19,7 +19,7 @@ function gameset(res) {
     if (res) {
         gameStartButton.classList.remove("hidden");
         answersBlock.classList.add("hidden");
-        gameStartButton.addEventListener("click", gameStart);
+        gameStartButton.addEventListener("click", gameStart, options);
     } else {
         gameStartButton.classList.add("hidden");
         answersBlock.classList.remove("hidden");
@@ -29,9 +29,12 @@ function gameset(res) {
 }
 
 function gameStart() {
-    console.log("game start2");
-    // scoreUpdate();
-    // questionNumUpdate();
+    console.log("game start");
+    scoreUpdate();
+    questionNumUpdate();
+    gameSet(false);
+    loadQuiz();
+
     // let gameContinue = true;
     // while (gameContinue) {
 
@@ -74,9 +77,38 @@ function questionNumUpdate(res) {
     }
 }
 
+function injectQuiz(q) {
 
+    let allAnswersLi = document.getElementsByClassName("answer-li");
+    let gameQuestion = document.getElementById("question");
+    gameQuestion.innerHTML = q.question;
 
+    for (let i = 0; i < allAnswersLi.length; i++) {
+        allAnswersLi[i].innerHTML = `<button class="answer" value="${Object.keys(q.answer)[i]}" data-ans="${q.correct}" type="button">${Object.keys(q.answer)[i].toUpperCase()}- <span>${q.answer[Object.keys(q.answer)[i]]}</span></button>`;
+    }
 
+    let allAnswers = document.getElementsByClassName("answer");
+    for (let item of allAnswers) {
+        item.addEventListener("click", showResult);
+    }
+
+}
+function checkAnswer(e) {
+    // console.log(this.value);
+    let correctAnswer = e.getAttribute("data-ans");
+    // console.log(correctAnswer);
+
+    return e.value === correctAnswer ? true : false;
+}
+
+function showResult() {
+    console.log(this);
+    if (checkAnswer(this)) {
+        console.log("yes");
+    } else {
+        console.log("no");
+    }
+}
 /**
  * Loads the make list with info from the data file
  */
@@ -84,11 +116,13 @@ function loadQuiz() {
     (async () => {
         const result = await getData();
         const data = await result.json();
-        questionNumber = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+        dataLength = data.length;
+        questionNumber = Math.floor(Math.random() * (dataLength));
+        console.log(questionNumber);
         currentQuestion = data[questionNumber];
-        console.log(currentQuestion);
-        // injectMade(getMake(data));
-        // injectModel(data);
+        // console.log(currentQuestion);
+        injectQuiz(currentQuestion);
+
     })();
 }
 
