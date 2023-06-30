c541c5651388335e9fac43a62c08120ba20df4f4
AhmadAlmasriDev <>
2023-06-30 14:23:52 +0000

    Add start playing functionality

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,50 +3,80 @@ const APILINK = "https://mocki.io/v1/5fb9ecc7-22b4-4ce0-91e9-f052d5c81b35";
 document.addEventListener("DOMContentLoaded", test);
 
 function test() {
-    debugger;
-    for (let i = 0; i < 10; i++) {
-        questionNumUpdate();
-    }
+    // debugger;
+    gameset(true);
 
 }
 
 
 
-function game() {
-    let gameContinue = True;
-
-    while (gameContinue) {
-
-        let gameQustionNum = document.getElementById("question-num");
-        let gameStart = document.getElementById("start");
-        let gameQustion = document.getElementById("question");
-        let gameAnswerA = document.getElementById("answer-a");
-        let gameAnswerB = document.getElementById("answer-b");
-        let gameAnswerC = document.getElementById("answer-c");
-        let gameAnswerD = document.getElementById("answer-d");
-
-
-
+function gameset(res) {
+    const options = {
+        once: true,
+    };
+    let gameStartButton = document.getElementById("start");
+    let answersBlock = document.getElementById("answers");
+    if (res) {
+        gameStartButton.classList.remove("hidden");
+        answersBlock.classList.add("hidden");
+        gameStartButton.addEventListener("click", gameStart);
+    } else {
+        gameStartButton.classList.add("hidden");
+        answersBlock.classList.remove("hidden");
     }
+
+
 }
+
+function gameStart() {
+    console.log("game start2");
+    // scoreUpdate();
+    // questionNumUpdate();
+    // let gameContinue = true;
+    // while (gameContinue) {
+
+
+    //     let gameQustionNum = document.getElementById("question-num");
+
+    //     let gameQustion = document.getElementById("question");
+    //     let gameAnswerA = document.getElementById("answer-a");
+    //     let gameAnswerB = document.getElementById("answer-b");
+    //     let gameAnswerC = document.getElementById("answer-c");
+    //     let gameAnswerD = document.getElementById("answer-d");
+
+
+
+    // }
+}
+
+
 function scoreUpdate(res) {
     let gameScoreCorrect = document.getElementById("correct-answers");
     let gameScoreWrong = document.getElementById("wrong-answers");
-
-
-    if (res === true) {
-        gameScoreCorrect.innerHTML = parseInt(gameScoreCorrect.innerHTML) + 1;
-
+    if (res) {
+        if (res === true) {
+            gameScoreCorrect.innerHTML = parseInt(gameScoreCorrect.innerHTML) + 1;
+        } else {
+            gameScoreWrong.innerHTML = parseInt(gameScoreWrong.innerHTML) + 1;
+        }
     } else {
-        gameScoreWrong.innerHTML = parseInt(gameScoreWrong.innerHTML) + 1;
+        gameScoreCorrect.innerHTML = "0";
+        gameScoreWrong.innerHTML = "0";
     }
-
 }
 
-function questionNumUpdate() {
+function questionNumUpdate(res) {
     let gameQustionNum = document.getElementById("question-num");
-    gameQustionNum.innerHTML = parseInt(gameQustionNum.innerHTML) + 1;
+    if (res) {
+        gameQustionNum.innerHTML = parseInt(gameQustionNum.innerHTML) + 1;
+    } else {
+        gameQustionNum.innerHTML = "0";
+    }
 }
+
+
+
+
 /**
  * Loads the make list with info from the data file
  */
