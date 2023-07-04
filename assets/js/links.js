435086bf38302fc4ce99a73d6e2ba42fd90ac3c1
AhmadAlmasriDev <>
2023-07-04 16:14:00 +0000

    Clean links js code

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", createListeners);
-
+/**
+ * Create event listeners for the links
+ */
 function createListeners() {
-
     let buttons = document.getElementsByClassName("links");
     for (let button of buttons) {
         // console.log(button.id);
@@ -9,14 +10,18 @@ function createListeners() {
         button.addEventListener("mouseleave", resetInfo);
     }
 }
-
+/**
+ * Update the info, content taken from aria-label
+ */
 function updateInfo(event) {
     let infoBlock = document.getElementById("link-info");
     // console.log(this.children.item(0).ariaLabel);
     // infoBlock.innerHTML = this.firstChild.ariaLabel;
     infoBlock.innerHTML = this.children.item(0).ariaLabel;
-
 }
+/**
+ * Reset the info content
+ */
 function resetInfo(event) {
     let infoBlock = document.getElementById("link-info");
     infoBlock.innerHTML = "Hover over the links below for more information.";
