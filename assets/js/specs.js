9ec76418e097ef81ddc8e1321deb9a32062f59a3
AhmadAlmasriDev <>
2023-07-05 18:55:24 +0000

    Update API link

--- assets/js/specs.js
+++ assets/js/specs.js
@@ -1,4 +1,4 @@
-const APILINK = "https://mocki.io/v1/f28fddb1-5b3b-45b9-8f17-7b7990639bc3";
+const APILINK = "https://mocki.io/v1/795b12cb-354b-446b-8991-036c44c0b681";
 
 document.addEventListener("DOMContentLoaded", LoadMakeList);
 
